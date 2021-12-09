1cc5408873a82cf99a278dff376f1f453a163883
Eman Hamza <eman_ebeid@hotmail.com>
2021-12-09 19:09:10 +0200

    commit changes

--- getUsersInfo.js
+++ getUsersInfo.js
@@ -35,20 +35,10 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 exports.__esModule = true;
-var readline = __importStar(require("readline"));
+var readline = require("readline");
 var promises_1 = require("fs/promises");
-var users_1 = __importDefault(require("./users"));
+var users_1 = require("./users");
 var json = require('./userData.json');
 var fs = require('fs');
 var data = JSON.parse(fs.readFileSync('./newData.json'));
